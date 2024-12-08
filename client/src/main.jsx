31fcda538a2588749afa745ec9c422e3bd5c6358
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-08 23:28:46 +0530

    fifth commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { Provider } from "react-redux"
+import { persistor, store } from "./redux/store.js"
+import { PersistGate } from "redux-persist/integration/react"
+import { BrowserRouter } from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+    </PersistGate>
+  </Provider>
 )
