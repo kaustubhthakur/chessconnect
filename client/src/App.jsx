e8f356e13426782a26d5daac204e5c5a000762f9
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-11 23:27:48 +0530

    fourth commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import {Route,Routes} from "react-router-dom"
+
 const App = () => {
   return (
-    <Routes>
-      <Route/>
-    </Routes>
+    <div>App</div>
   )
 }
 
