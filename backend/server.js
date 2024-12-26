14990a2b0b1b23adc9316393ce9b8255584c1379
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-26 23:14:33 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,30 @@
 const express = require('express')
 const app = express();
 const port = 9000;
-const authrouter= require('./routes/auth')
-const userrouter = require('./routes/users'
-
-)
+const authrouter = require('./routes/auth')
+const userrouter = require('./routes/users')
 const postrouter = require('./routes/posts')
 const cors = require('cors')
-const cookieparser= require('cookie-parser')
+const cookieparser = require('cookie-parser')
 const mongoose = require('mongoose')
 require('dotenv').config();
 app.use(express.json())
 app.use(cookieparser())
 app.use(cors());
-const connection = async()=> 
-{
- try {
-    await mongoose.connect(process.env.MONGODB);
-    console.log('db is connected...')
- } catch (error) {
-    console.error(error);
- }   
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log('db is connected...')
+    } catch (error) {
+        console.error(error);
+    }
 }
 connection()
 
-app.use('/auth',authrouter);
-app.use('/posts',postrouter)
-app.use('/users',userrouter)
-app.listen(port,() => {
+app.use('/auth', authrouter);
+app.use('/posts', postrouter)
+app.use('/users', userrouter)
+app.listen(port, () => {
     console.log(
         `server is running on port ${port}...`
     )
