00bdc5109e1e4be80027952b745d1158ea7d4118
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-26 23:00:48 +0530

    third commit

--- client/src/pages/registerpage/RegisterPage.jsx
+++ client/src/pages/registerpage/RegisterPage.jsx
@@ -1,85 +1,9 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./RegisterPage.css";
+import React from 'react'
 
 const RegisterPage = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const setUser = useSetRecoilState(userAtom);
-
-  const handleSignup = async () => {
-    try {
-        const res = await fetch("/api/users/signup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(inputs),
-        });
-        const data = await res.json();
-
-        if (data.error) {
-            showToast("Error", data.error, "error");
-            return;
-        }
-
-        localStorage.setItem("user-threads", JSON.stringify(data));
-        setUser(data);
-        alert('user registered...')
-    } catch (error) {
-        showToast("Error", error, "error");
-    }
-};
-
   return (
-    <div className="register-page">
-      <form className="register-form" onSubmit={handleSubmit}>
-        <h2 className="form-title">Register</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-            placeholder="Enter your username"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            placeholder="Enter your email"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-            placeholder="Enter your password"
-          />
-        </div>
-        <button type="submit" className="submit-button">
-          Register
-        </button>
-      </form>
-    </div>
-  );
-};
+    <div>RegisterPage</div>
+  )
+}
 
-export default RegisterPage;
+export default RegisterPage
