5cfd667c29ba4bcfaebd012b68b8364305a1be48
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-31 22:27:13 +0530

    first commit

--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const protectRoute = require('../utils/protectRoute')
-const {followUnFollowUser,updateUser,getAllUsers,getUserProfile} = require('../controllers/users')
-router.put('/:id/follow',protectRoute,followUnFollowUser)
-router.put('/:id',protectRoute,updateUser)
-router.get('/:id',protectRoute,getUserProfile)
-router.get('/',getAllUsers)
-module.exports = router;
