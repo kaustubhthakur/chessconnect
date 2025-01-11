98cf8fe7a42e1b83c2bffb4c367f80ea5da3da00
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-11 23:42:01 +0530

    first commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from "react-router-dom"
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import { RecoilRoot } from "recoil";
 
-createRoot(document.getElementById('root')).render(
-  <RecoilRoot> 
+createRoot(document.getElementById("root")).render(
+  <RecoilRoot>
     <BrowserRouter>
-    <App />
-  </BrowserRouter>
+      <App />
+    </BrowserRouter>
   </RecoilRoot>
-
-)
+);
